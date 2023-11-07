6303386c087141e71c4535d8abea7415966a8013
lyoung <lashellyoung15@gmail.com>
2023-11-07 13:12:55 -0600

    update activities page & refactored selection btn

--- src/components/entertainment/EntertainmentList.js
+++ src/components/entertainment/EntertainmentList.js
@@ -3,126 +3,39 @@ import React, { useEffect, useState } from 'react'
 import './Entertainment.css'
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
 import Grow from '@mui/material/Grow';
 import Paper from '@mui/material/Paper';
 import Popper from '@mui/material/Popper';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
-import TextField from '@mui/material/TextField';
 
-// const options = ['Title', 'Type', 'Genre', 'Rating'];
-const options = ['Action', 'Comedy', 'Crime', 'Suspense', 'Sci-fi', 'History',
+const options = ['All', 'Action', 'Comedy', 'Crime', 'Suspense', 'Sci-fi', 'History',
     'Romance', 'Gay', 'Thriller', 'Drama', 'Western', 'Adventure', 'Fantasy', 'Mystery',
     'Animation', 'Period piece', 'Spanish', 'Horror'];
 
 const EntertainmentList = ({ entertainment }) => {
-    // console.log("entertainment in EntertainmentList")
-    // console.log(entertainment)
     const [selectedItems, setSelectedItems] = useState();
     const [open, setOpen] = useState(false);
     const anchorRef = React.useRef(null);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [searchInput, setSearchInput] = useState("");
 
-    const getEntertainmentByGenre = async (option) => {
+    const getEntertainmentByGenre = (index) => {
         try {
-            // const response = await api.get(`/api/entertainment/genre?genres=${searchInput}`);
-            // setSelectedItems(response.data);
-            const byGenre = entertainment.filter((item) => {
-                // let genres = searchInput.split(", ");
-
-                // for (let i = 0; i < genres.length; i++) {
-                if (item.genres.includes(option.toLowerCase())) {
-                    return true;
-                }
-                // }
-            })
-
-            setSelectedItems(byGenre);
+            setOpen(false);
+            if (options[index] == 'All') {
+                setSelectedItems(entertainment);
+            } else {
+                const byGenre = entertainment.filter((item) => {
+                    if (item.genres.includes(options[index].toLowerCase())) {
+                        return true;
+                    }
+                })
+                setSelectedItems(byGenre);
+            }
         } catch (err) {
             console.log(err);
         }
     }
-    const getEntertainmentByTitle = async () => {
-        try {
-            // const response = await api.get(`/api/entertainment/title/${searchInput}`);
-            // setSelectedItems(response.data);
-
-            const byTitle = entertainment.filter((item) => {
-                let search = searchInput[0].toUpperCase() + searchInput.substring(1);
-                return item.title.includes(search) + item.title.includes(searchInput);
-            })
-
-            setSelectedItems(byTitle);
-            // console.log(response.data);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-    const getEntertainmentByType = async () => {
-        try {
-            // const response = await api.get(`/api/entertainment/type/${searchInput}`);
-            // setSelectedItems(response.data);
-            // console.log(response.data);
-
-            const byType = entertainment.filter((item) => {
-                return item.type == searchInput.toLowerCase();
-            })
-
-            setSelectedItems(byType);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-    const getEntertainmentByRating = async () => {
-        try {
-            // const response = await api.get(`/api/entertainment/rate/${searchInput}`);
-            // setSelectedItems(response.data);
-            // console.log(response.data);
-            const byRating = entertainment.filter((item) => {
-                return item.rating == searchInput;
-            })
-
-            setSelectedItems(byRating);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    const handleClick = (option, index) => {
-        setSelectedIndex(index);
-        setOpen(false);
-        // call api using user input
-        if (searchInput == "") {
-            setSelectedItems(entertainment);
-        } else {
-            getEntertainmentByGenre(option);
-            // switch (selectedIndex) {
-            //     case 0:
-            //         getEntertainmentByTitle();
-            //         break;
-            //     case 1:
-            //         getEntertainmentByType();
-            //         break;
-            //     case 2:
-            //         getEntertainmentByGenre();
-            //         break;
-            //     case 3:
-            //         if (parseInt(searchInput) > 0 && parseInt(searchInput) <= 5) {
-            //             getEntertainmentByRating();
-            //         }
-            //         break;
-            // }
-        }
-        console.log(`You clicked ${options[selectedIndex]} with ${selectedIndex}`);
-    };
-
-    const handleMenuItemClick = (event, index) => {
-        setSelectedIndex(index);
-        setOpen(false);
-    };
 
     const handleToggle = () => {
         setOpen((prevOpen) => !prevOpen);
@@ -136,31 +49,10 @@ const EntertainmentList = ({ entertainment }) => {
         setOpen(false);
     };
 
-    const handleChange = (event) => {
-        setSearchInput(event.target.value);
-    }
-
-    const setLabel = (index) => {
-        switch (index) {
-            case 1:
-                return 'Enter "movie" or "series"';
-                break;
-            case 2:
-                return 'Enter comma separate values';
-                break;
-            case 3:
-                return 'Enter a whole number 1-5';
-            default:
-                return `Enter ${options[index]}`;
-        }
-    }
-
     useEffect(() => {
         setSelectedItems(entertainment);
     }, [])
 
-    // console.log("selectedItems")
-    // console.log(selectedItems)
     return (
         <>
             <div style={{ margin: "30px" }}>
@@ -190,9 +82,8 @@ const EntertainmentList = ({ entertainment }) => {
                                     <MenuList id="split-button-menu" autoFocusItem>
                                         {options.map((option, index) => (
                                             <MenuItem
-                                                key={option}
-                                                selected={index === selectedIndex}
-                                                onClick={(event) => handleClick(event, index)}
+                                                key={index}
+                                                onClick={(event) => getEntertainmentByGenre(index)}
                                             >
                                                 {option}
                                             </MenuItem>
@@ -206,7 +97,7 @@ const EntertainmentList = ({ entertainment }) => {
             </div>
             <div className='containter'>
                 <div className='row justify-content-center'>
-                    {selectedItems !== undefined && selectedItems !== [] ?
+                    {selectedItems !== undefined && selectedItems.length > 0 ?
                         selectedItems.map((item) => {
                             return (
                                 <div className='col-sm-6 col-md-3' key={item}>
--- src/data/activities.js
+++ src/data/activities.js
@@ -1,106 +1,32 @@
 export const activities = [
   {
-    "name": "Camping",
-    "type": "active",
-    "rating": null,
-    "description": "the act of staying and sleeping in an outside area for one or more days and nights, usually in a tent.",
-    "details": "",
-    "link": ""
+    "title": "Puttery",
+    "type": "mini golf & bar",
+    "trailerLink": "https://www.puttery.com/locations/dallas",
+    "genres": ['active', 'nightlife'],
+    "poster": "https://www.themoviedb.org/t/p/original/5TblBHvORO9yplo0ZM7zi4KjL7c.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
   },
   {
-    "name": "Dinosaur Valley State Park",
-    "type": "park",
-    "rating": null,
-    "description": "Dinosaur Valley State Park is a state park near Glen Rose, Texas, United States.",
-    "details": "Address: Glen Rose, TX 76043",
-    "link": "https://tpwd.texas.gov/state-parks/dinosaur-valley"
+    "title": "Camping",
+    "type": "spending time in nature",
+    "trailerLink": "https://tpwd.texas.gov/state-parks/dinosaur-valley",
+    "genres": ['active'],
+    "poster": "https://www.themoviedb.org/t/p/original/rSje3FS7ycJSglowlngjsvDt7vO.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
   },
   {
-    "name": "Paddle boarding",
-    "type": "active",
-    "rating": 4,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Ziplining",
-    "type": "active",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Renaissance fair",
-    "type": "attraction",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "City fair",
-    "type": "attraction",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Ferris wheel",
-    "type": "attraction",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "rock climbing",
-    "type": "active",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Trip to Greece",
-    "type": "travel",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Trip to Colorado",
-    "type": "travel",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Karaoke bar",
-    "type": "nightlife",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Roller skating",
-    "type": "active",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
-  },
-  {
-    "name": "Hiking",
-    "type": "active",
-    "rating": 0,
-    "description": "",
-    "details": "",
-    "link": ""
+    "title": "Dallas after dark zoo lights",
+    "type": "A limited time event at the Dallas Zoo",
+    "trailerLink": "https://www.dallaszoo.com/",
+    "genres": ['attraction'],
+    "poster": "https://www.themoviedb.org/t/p/original/sERAdCJCu0boxQFKpFC831zBErC.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
   }
 ]
