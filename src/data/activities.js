15e1b714f57f417d359d823d232a815d6761921e
lyoung <lashellyoung15@gmail.com>
2023-11-14 19:29:08 -0600

    added more activities

--- src/data/activities.js
+++ src/data/activities.js
@@ -3,15 +3,15 @@ export const activities = [
     "title": "Puttery",
     "type": "mini golf & bar",
     "trailerLink": "https://www.puttery.com/locations/dallas",
-    "genres": ['active', 'nightlife'],
+    "genres": ['active', 'drinking will occur'],
     "poster": "https://www.themoviedb.org/t/p/original/5TblBHvORO9yplo0ZM7zi4KjL7c.jpg",
     "backdrops": [],
     "rating": 3,
     "hasWatched": false
   },
   {
-    "title": "Camping",
-    "type": "spending time in nature",
+    "title": "Dinosaur Park",
+    "type": "spending time in nature at this place",
     "trailerLink": "https://tpwd.texas.gov/state-parks/dinosaur-valley",
     "genres": ['active'],
     "poster": "https://www.themoviedb.org/t/p/original/rSje3FS7ycJSglowlngjsvDt7vO.jpg",
@@ -23,8 +23,60 @@ export const activities = [
     "title": "Dallas after dark zoo lights",
     "type": "A limited time event at the Dallas Zoo",
     "trailerLink": "https://www.dallaszoo.com/",
-    "genres": ['attraction'],
+    "genres": ['attraction', 'romantic', 'costly'],
     "poster": "https://www.themoviedb.org/t/p/original/sERAdCJCu0boxQFKpFC831zBErC.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
+  },
+  {
+    "title": "Electric shuffle",
+    "type": "Bar games, Live performances",
+    "trailerLink": "https://www.google.com/maps/place/Electric+Shuffle+Dallas/@32.779703,-96.7864353,14.63z/data=!4m15!1m8!3m7!1s0x864e99368b65fa2b:0x9d19bdcf22b74fbe!2sElectric+Shuffle+Dallas!8m2!3d32.7844768!4d-96.7859195!10e1!16s%2Fg%2F11rsv9vsv1!3m5!1s0x864e99368b65fa2b:0x9d19bdcf22b74fbe!8m2!3d32.7844768!4d-96.7859195!16s%2Fg%2F11rsv9vsv1?entry=ttu",
+    "genres": ['nightlife', 'drinking will occur'],
+    "poster": "https://www.themoviedb.org/t/p/original/9MGCeg4EQXklLAeAB0JPTerNynI.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
+  },
+  {
+    "title": "Museum of sex",
+    "type": "Intimate museum chronicling the evolution of human sexuality in ever-changing exhibits",
+    "trailerLink": "https://www.google.com/maps/place/Museum+of+Sex/@40.7440331,-73.9898278,17z/data=!3m2!4b1!5s0x89c259a642ed91ef:0x3dd1e8a751a1047c!4m6!3m5!1s0x89c259a642ba4d47:0xa3204d864887bb80!8m2!3d40.7440291!4d-73.9872529!16zL20vMDlzang1?entry=ttu",
+    "genres": ['travel', 'romantic', 'kiss ya money goodbye'],
+    "poster": "https://www.themoviedb.org/t/p/original/rRvdtpCUuAnzyVcYJvNDCZsBL0z.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
+  },
+  // 'Active', 'Romantic', 'Free', 'Inexpensive', 'Drinking Will Occur', 'costly', 'kiss ya money goodbye',
+  // 'Attraction', 'Travel', 'Educational', 'Nightlife', 'Hide From The World Together'
+  {
+    "title": "Andretti's",
+    "type": "arcade, laser tag, go karts",
+    "trailerLink": "https://www.google.com/maps/place/Andretti+Indoor+Karting+%26+Games+The+Colony/@33.0742537,-96.8637275,17z/data=!3m1!4b1!4m6!3m5!1s0x864c3ba02b553647:0x76840569d19af972!8m2!3d33.0742492!4d-96.8611526!16s%2Fg%2F11j0kgfhrw?entry=ttu",
+    "genres": ['active', 'attraction', 'costly'],
+    "poster": "https://www.themoviedb.org/t/p/original/qgyxx3kppbL326Q2yEu5pCqLIvg.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
+  },
+  {
+    "title": "Camping",
+    "type": "spending time in nature",
+    "trailerLink": "https://www.google.com/maps/@32.9976259,-97.1715677,12z/data=!3m1!4b1!4m3!11m2!2sIIKBtJskSXy_qRkqhaxjZQ!3e3?authuser=0&entry=ttu",
+    "genres": ['active', 'hide from the world together'],
+    "poster": "https://www.themoviedb.org/t/p/original/pgffCA82hmBhavW23nbipzsmYf5.jpg",
+    "backdrops": [],
+    "rating": 3,
+    "hasWatched": false
+  },
+  {
+    "title": "Harry Potter Enchanted Forest",
+    "type": "Harry Potter Enchanted Forest",
+    "trailerLink": "https://hpforbiddenforestexperience.com/",
+    "genres": ['attraction', 'kiss ya money goodbye'],
+    "poster": "https://www.themoviedb.org/t/p/original/wuMc08IPKEatf9rnMNXvIDxqP4W.jpg",
     "backdrops": [],
     "rating": 3,
     "hasWatched": false
